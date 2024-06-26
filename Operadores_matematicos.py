a3b02eab9d28b32a495a22b6f8853eba9ad0c418
DiegoSarti <163166022+DiegoSarti@users.noreply.github.com>
2024-06-25 21:31:47 -0300

    Add files via upload

---
+++ Operadores_matematicos.py
@@ -0,0 +1,29 @@
+# ================================================================================================================
+#Autor: Diego Sarti Araujo Viana
+# Data: 25/05/2024
+# Verão: 1.0
+# Descrição; Trabalhar com as quatro operações matematicas
+#            + (soma)
+#            - (menos)
+#            * (multiplicação)
+#            / (divisão)
+# ==================================================================================================================
+# inicio
+# variaveis
+# substituir para entrada do Usuario (input)
+numeroUm = 10
+numeroDois = 5
+resultado = 0 #variavel para receber o resultado da operação
+# soma
+resultado = numeroUm + numeroDois
+print(numeroUm,'+', numeroDois,'=', resultado)
+# subtração
+resultado = numeroUm - numeroDois
+print(numeroUm,'-', numeroDois,'=',resultado)
+# multiplicação
+resultado = numeroUm * numeroDois
+print(numeroUm,'*', numeroDois,'=',resultado)
+#divisão
+resultado = numeroUm / numeroDois
+print(numeroUm,'/', numeroDois,'=',resultado)
+# fim
