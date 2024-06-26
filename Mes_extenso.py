1a2b5a6079197bb22916b463907a18c544c20ffb
DiegoSarti <163166022+DiegoSarti@users.noreply.github.com>
2024-06-25 21:37:08 -0300

    Add files via upload

---
+++ Mes_extenso.py
@@ -0,0 +1,30 @@
+
+print('=======================================================================================================')
+print('insira qual mes vc deseja selecionar:')
+print('Nr. Mês \n1.janeiro \n2.Fevereiro \n3.Março \n4.Abril \n5.maio \n6.junho \n7.julho \n8.Agosto \n9.Setembro \n10.Outubro \n11.Novembro \n12.Dezembro')
+mes = int(input('qual mes voe deseja selecionar: '))
+
+if mes == 1:
+     print("Janeiro")
+elif mes == 2:
+     print('Fevereiro')
+elif mes == 3:
+     print('Março')
+elif mes == 4:
+     print('Abril')
+elif mes == 5:
+     print('maio')
+elif mes == 6:
+     print('Junho')
+elif mes == 7:
+     print('Julho')
+elif mes == 8:
+     print('Agosto')
+elif mes == 9:
+     print('Setembro')
+elif mes == 10:
+     print('Outubro')
+elif mes == 11:
+     print('Novembro')
+elif mes == 12:
+     print('Dezembro')
